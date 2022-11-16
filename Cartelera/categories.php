c522b3e62b28138f9fd36a53029871e53d4af317
Georgie <ghbenevento@gmail.com>
2022-11-16 17:53:06 +0100

    Started cartelera skeleton

--- Cartelera/categories.php
+++ Cartelera/categories.php
@@ -4,23 +4,26 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/categories.css">
     <title>Cartelera</title>
-    
+
 </head>
 <body>
-    <div class="container">
+    
+    <div class="large_conatiner">
         <div id="header">
             <h1>Cartelera</h1>
-
         </div>
-
-        <div id="content">
-            <h1>Contenido</h1>
+        <div class="container">
+            <ul>
+                <li><img src="imgs/pulp_fiction.jpg"></li>
+                <li><img></li>
+            </ul>
         </div>
-
         <div id="footer">
             <h1>Footer</h1>
         </div>
     </div>
+
 </body>
 </html>
